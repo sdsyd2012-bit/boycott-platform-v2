884eb702c0453ad3de00b9a8f038ce77b3f27a4f
kali <kali@kali.kali>
2026-08-15 17:41:42 -0400

    Add auto superuser creation to build script

--- build.sh
+++ build.sh
@@ -13,3 +13,6 @@ python manage.py ensure_media || true
 
 python manage.py migrate --noinput
 python manage.py loaddata data
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin12345')"
+
